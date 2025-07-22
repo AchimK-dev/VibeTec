39ac43db36d62a91e98c820b0582288e25c10567
Achim Klein <achimklein95@gmail.com>
2025-07-22 21:48:25 +0200

    fix: for render

--- server/index.js
+++ server/index.js
@@ -20,6 +20,10 @@ app.use(cookieParser());
  */
 app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));
 app.use(express.json());
+app.use((req, res, next) => {
+  console.log(`📨 [${req.method}] ${req.url}`);
+  next();
+});
 app.use('/api/auth', authRouter);
 app.use('/api/artists', artistRouter);
 app.use('/api/bookings', bookingRouter);
