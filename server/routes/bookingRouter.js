3f58d221716e754121a36f79c38c7444be436a77
Achim Klein <achimklein95@gmail.com>
2025-07-22 22:02:15 +0200

    fix: for render

--- server/routes/bookingRouter.js
+++ server/routes/bookingRouter.js
@@ -13,15 +13,13 @@ import verifyAdmin from '../middlewares/verifyAdmin.js';
 
 const bookingRouter = Router();
 
-// User booking routes (require authentication only) - MUST come first!
-bookingRouter.get('/my-bookings', verifyToken, getUserBookings);
-bookingRouter.patch('/:bookingId', verifyToken, updateUserBooking);
-bookingRouter.delete('/:bookingId', verifyToken, deleteUserBooking);
-
-// Admin booking routes (require admin privileges)
 bookingRouter.get('/', verifyToken, verifyAdmin, getAllBookings);
 bookingRouter.get('/pending', verifyToken, verifyAdmin, getPendingBookings);
 bookingRouter.patch('/confirm/:artistId/:bookingId', verifyToken, verifyAdmin, confirmBooking);
 bookingRouter.delete('/reject/:artistId/:bookingId', verifyToken, verifyAdmin, rejectBooking);
+
+bookingRouter.get('/my-bookings', verifyToken, getUserBookings);
+bookingRouter.patch('/:bookingId', verifyToken, updateUserBooking);
+bookingRouter.delete('/:bookingId', verifyToken, deleteUserBooking);
 
 export default bookingRouter;
